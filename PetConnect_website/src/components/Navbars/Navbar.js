305a533d0de0ed6ea072343f81a7e0ec1fd10d1f
strasekeva <eva.strasek@student.um.si>
2024-05-18 10:20:45 +0200

    Tamplate spletne strani

---
+++ PetConnect_website/src/components/Navbars/Navbar.js
@@ -0,0 +1,238 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import { Link } from "react-router-dom";
+// JavaScript plugin that hides or shows a component based on your scroll
+import Headroom from "headroom.js";
+// reactstrap components
+import {
+  Button,
+  UncontrolledCollapse,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle,
+  UncontrolledDropdown,
+  Media,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip,
+} from "reactstrap";
+
+class DemoNavbar extends React.Component {
+  componentDidMount() {
+    let headroom = new Headroom(document.getElementById("navbar-main"));
+    // initialise
+    headroom.init();
+  }
+  state = {
+    collapseClasses: "",
+    collapseOpen: false,
+  };
+
+  onExiting = () => {
+    this.setState({
+      collapseClasses: "collapsing-out",
+    });
+  };
+
+  onExited = () => {
+    this.setState({
+      collapseClasses: "",
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <header className="header-global">
+          <Navbar
+            className="navbar-main navbar-transparent navbar-light headroom"
+            expand="lg"
+            id="navbar-main"
+          >
+            <Container>
+              <NavbarBrand className="mr-lg-5" to="/" tag={Link}>
+                <p>PETCONNECT</p>
+              </NavbarBrand>
+              <button className="navbar-toggler" id="navbar_global">
+                <span className="navbar-toggler-icon" />
+              </button>
+              <UncontrolledCollapse
+                toggler="#navbar_global"
+                navbar
+                className={this.state.collapseClasses}
+                onExiting={this.onExiting}
+                onExited={this.onExited}
+              >
+                <div className="navbar-collapse-header">
+                  <Row>
+                    <Col className="collapse-brand" xs="6">
+                      <Link to="/">
+                        <img
+                          alt="..."
+                          src={require("assets/img/brand/argon-react.png")}
+                        />
+                      </Link>
+                    </Col>
+                    <Col className="collapse-close" xs="6">
+                      <button className="navbar-toggler" id="navbar_global">
+                        <span />
+                        <span />
+                      </button>
+                    </Col>
+                  </Row>
+                </div>
+                <Nav className="navbar-nav-hover align-items-lg-center" navbar>
+                  <UncontrolledDropdown nav>
+                  <DropdownToggle nav>
+                      <i className="ni ni-collection d-lg-none mr-1" />
+                      <span className="nav-link-inner--text">Skupnost</span>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem to="#" tag={Link}>
+                      Skupinske aktivnosti
+                      </DropdownItem>
+                      <DropdownItem to="#" tag={Link}>
+                      Forum
+                      </DropdownItem>
+                      <DropdownItem to="#" tag={Link}>
+                      Lokalne skupine
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                  <UncontrolledDropdown nav>
+                    <DropdownToggle nav>
+                      <i className="ni ni-collection d-lg-none mr-1" />
+                      <span className="nav-link-inner--text">Priporočila</span>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem to="#" tag={Link}>
+                      Hrana
+                      </DropdownItem>
+                      <DropdownItem to="#" tag={Link}>
+                      Igrače
+                      </DropdownItem>
+                      <DropdownItem to="#" tag={Link}>
+                      Pripomočki
+                      </DropdownItem>
+                      <DropdownItem to="#" tag={Link}>
+                      Izdelki za nego
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                  <UncontrolledDropdown nav>
+                  <DropdownToggle nav>
+                      <i className="ni ni-collection d-lg-none mr-1" />
+                      <span className="nav-link-inner--text">Veterinarski nasveti</span>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem to="#" tag={Link}>
+                      Članki
+                      </DropdownItem>
+                      <DropdownItem to="#" tag={Link}>
+                      Nasveti
+                      </DropdownItem>
+                      <DropdownItem to="#" tag={Link}>
+                      Pogosta vprašanja
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                  <UncontrolledDropdown nav>
+                  <DropdownToggle nav>
+                      <i className="ni ni-collection d-lg-none mr-1" />
+                      <span className="nav-link-inner--text">Zdravje</span>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem to="#" tag={Link}>
+                      Cepljenja
+                      </DropdownItem>
+                      <DropdownItem to="#" tag={Link}>
+                      Prehrana
+                      </DropdownItem>
+                      <DropdownItem to="#" tag={Link}>
+                      Obiski pri veterinarju
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                  <UncontrolledDropdown nav>
+                  <DropdownToggle nav>
+                      <i className="ni ni-collection d-lg-none mr-1" />
+                      <span className="nav-link-inner--text">Oskrba</span>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem to="#" tag={Link}>
+                      Sprehajalci
+                      </DropdownItem>
+                      <DropdownItem to="#" tag={Link}>
+                      Frizerji
+                      </DropdownItem>
+                      <DropdownItem to="#" tag={Link}>
+                      Trenerji
+                      </DropdownItem>
+                      <DropdownItem to="#" tag={Link}>
+                      Varstvo ljubljenčkov
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </Nav>
+                <Nav className="ml-lg-auto" navbar>
+                  <NavItem>
+                    <NavLink to="/register-page" tag={Link}>
+                      Registracija <span className="sr-only">(current)</span>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink to="/login-page" tag={Link}>
+                      Prijava
+                    </NavLink>
+                  </NavItem>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav><i className="ni ni-circle-08"></i></DropdownToggle>
+                    <DropdownMenu
+                      aria-labelledby="navbar-primary_dropdown_1"
+                      right
+                    >
+                      <DropdownItem to="/profile-page" tag={Link}>
+                        Uredi profil
+                      </DropdownItem>
+                      <DropdownItem divider />
+                      <DropdownItem
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        Odjava
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </Nav>
+              </UncontrolledCollapse>
+            </Container>
+          </Navbar>
+        </header>
+      </>
+    );
+  }
+}
+
+export default DemoNavbar;
