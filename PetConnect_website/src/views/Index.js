cea66fe3c8216e92cea2aa7b0c3bfb3d75510dc4
anjaluzar <anja.luzar1@student.um.si>
2024-05-29 20:29:05 +0200

    Posodobljena prva stran

--- PetConnect_website/src/views/Index.js
+++ PetConnect_website/src/views/Index.js
@@ -1,20 +1,4 @@
-/*!
 
-=========================================================
-* Argon Design System React - v1.1.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-design-system-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 
 // reactstrap components
@@ -55,81 +39,83 @@ class Index extends React.Component {
   }
   render() {
     return (
-      <>
-        <Navbar />
-        <main ref="main">
-          <Hero />
-          <section id="funkcionalnosti" className="section section-lg">
-            <Container>
-              <Row>
-            <Col lg="4" className="mb-5 mb-lg-0">
-                  <div className="feature-box shadow-lg rounded-lg text-center p-4">
+        <>
+          <Navbar />
+          <main ref="main">
+            <Hero />
+            <section id="funkcionalnosti" className="section section-lg reduce-footer-margin">
+              <Container>
+                <Row>
+                  <Col lg="4" className="mb-5 mb-lg-0">
+                    <div className="feature-box shadow-lg rounded-lg text-center p-4">
                       <div className="icon icon-lg rounded-circle">
-                          <i className="ni ni-chat-round"></i>
+                        <i className="ni ni-chat-round"></i>
                       </div>
-                      <h3 className="mt-4" style={{fontSize: "1.5rem", fontWeight: "bold", color: "#333333"}}>Skupnost</h3>
+                      <h3 className="mt-4" style={{ fontSize: "1.5rem", fontWeight: "bold", color: "#333333" }}>Skupnost</h3>
                       <p className="mt-3">
-                          Povežite se s skupnostjo preko skupinskih aktivnosti, foruma in lokalnih skupin.
+                        Povežite se s skupnostjo preko skupinskih aktivnosti, foruma in lokalnih skupin.
                       </p>
                       <a href="/skupinske-aktivnosti" className="btn btn-primary mt-4">Več informacij</a>
-                  </div>
-              </Col>
-              <Col lg="4" className="mb-5 mb-lg-0">
-                  <div className="feature-box shadow-lg rounded-lg text-center p-4">
+                    </div>
+                  </Col>
+                  <Col lg="4" className="mb-5 mb-lg-0">
+                    <div className="feature-box shadow-lg rounded-lg text-center p-4">
                       <div className="icon icon-lg rounded-circle">
-                          <i className="ni ni-shop"></i>
+                        <i className="ni ni-shop"></i>
                       </div>
-                      <h3 className="mt-4" style={{fontSize: "1.5rem", fontWeight: "bold", color: "#333333"}}>Priporočila izdelkov</h3>
+                      <h3 className="mt-4" style={{ fontSize: "1.5rem", fontWeight: "bold", color: "#333333" }}>Priporočila izdelkov</h3>
                       <p className="mt-3">
-                          Odkrijte najboljše izdelke za vašega ljubljenčka, ki jih priporoča naša skupnost.
+                        Odkrijte najboljše izdelke za vašega ljubljenčka, ki jih priporoča naša skupnost.
                       </p>
                       <a href="/izdelki" className="btn btn-primary mt-4">Več informacij</a>
-                  </div>
-              </Col>
-              <Col lg="4">
-                  <div className="feature-box shadow-lg rounded-lg text-center p-4">
+                    </div>
+                  </Col>
+                  <Col lg="4">
+                    <div className="feature-box shadow-lg rounded-lg text-center p-4">
                       <div className="icon icon-lg rounded-circle">
-                          <i className="ni ni-ambulance"></i>
+                        <i className="ni ni-ambulance"></i>
                       </div>
-                      <h3 className="mt-4" style={{fontSize: "1.5rem", fontWeight: "bold", color: "#333333"}}>Veterinarski nasveti</h3>
+                      <h3 className="mt-4" style={{ fontSize: "1.5rem", fontWeight: "bold", color: "#333333" }}>Veterinarski nasveti</h3>
                       <p className="mt-3">
-                          Dostopajte do strokovnih nasvetov, člankov in možnosti klepetanja s ChatGPT-jem.
+                        Dostopajte do strokovnih nasvetov, člankov in možnosti klepetanja s ChatGPT-jem.
                       </p>
                       <a href="/clanki" className="btn btn-primary mt-4">Več informacij</a>
-                  </div>
-              </Col>
-              <Col lg="4" className="mb-5 mb-lg-0">
-                  <div className="feature-box shadow-lg rounded-lg text-center p-4">
+                    </div>
+                  </Col>
+                </Row>
+                <Row className="mt-5 justify-content-center">
+                  <Col lg="4" className="mb-5 mb-lg-0">
+                    <div className="feature-box shadow-lg rounded-lg text-center p-4">
                       <div className="icon icon-lg rounded-circle">
-                          <i className="ni ni-favourite-28"></i>
+                        <i className="ni ni-favourite-28"></i>
                       </div>
-                      <h3 className="mt-4" style={{fontSize: "1.5rem", fontWeight: "bold", color: "#333333"}}>Zdravje</h3>
+                      <h3 className="mt-4" style={{ fontSize: "1.5rem", fontWeight: "bold", color: "#333333" }}>Zdravje</h3>
                       <p className="mt-3">
-                          Sledite zdravju vašega ljubljenčka s sledenjem cepljenjem, prehrani in obiskov pri veterinarju.
+                        Sledite zdravju vašega ljubljenčka s sledenjem cepljenjem, prehrani in obiskov pri veterinarju.
                       </p>
                       <a href="/sledenje-zdravju" className="btn btn-primary mt-4">Več informacij</a>
-                  </div>
-              </Col>
-              <Col lg="4" className="mb-5 mb-lg-0">
-                  <div className="feature-box shadow-lg rounded-lg text-center p-4">
+                    </div>
+                  </Col>
+                  <Col lg="4" className="mb-5 mb-lg-0">
+                    <div className="feature-box shadow-lg rounded-lg text-center p-4">
                       <div className="icon icon-lg rounded-circle">
-                          <i className="ni ni-calendar-grid-58"></i>
+                        <i className="ni ni-calendar-grid-58"></i>
                       </div>
-                      <h3 className="mt-4" style={{fontSize: "1.5rem", fontWeight: "bold", color: "#333333"}}>Oskrba</h3>
+                      <h3 className="mt-4" style={{ fontSize: "1.5rem", fontWeight: "bold", color: "#333333" }}>Oskrba</h3>
                       <p className="mt-3">
-                          Rezervirajte storitve, kot so frizer, veterinar, hoteli in sprehodi, za vašega ljubljenčka.
+                        Rezervirajte storitve, kot so frizer, veterinar, hoteli in sprehodi, za vašega ljubljenčka.
                       </p>
                       <a href="/koledar" className="btn btn-primary mt-4">Več informacij</a>
-                  </div>
-              </Col>
-              </Row>
-            </Container>
-          </section>
-        </main>
-        <CardsFooter />
-      </>
-    );
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            </section>
+          </main>
+          <CardsFooter />
+        </>
+      );
+    }
   }
-}
-
-export default Index;
+  
+  export default Index;
