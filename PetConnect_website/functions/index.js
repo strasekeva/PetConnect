21d5c7059619e537449c4729d8cc80554cedf900
strasekeva <eva.strasek@student.um.si>
2024-06-06 20:46:33 +0200

    Deploy

--- PetConnect_website/functions/index.js
+++ PetConnect_website/functions/index.js
@@ -1,19 +1,13 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// Dynamically import the server module
+import('./server.mjs').then((module) => {
+    const api = module.default; // Obtain the exported app from the server module
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+    // Export the Firebase function after the module has been successfully loaded
+    exports.api = functions.https.onRequest(api);
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+}).catch(err => {
+    console.error('Failed to load the server module:', err);
+    // Handle failures where the server might not load
+});
